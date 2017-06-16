1c8d486c4255ca4b85cadebc15bd7c2ea8311299
Elianagam <elianagam2@gmail.com>
2017-06-15 22:37:09 -0300

    tp

---
+++ Grafo.py
@@ -0,0 +1,77 @@
+import random
+
+class Grafo:
+	"""implementacion de clase grafo"""
+	def __init__(self, bool_dirigido):
+		"""..."""
+		self.grafo = {}
+		self.dirigido = bool_dirigido 
+
+	def adyacentes(self, vertice):
+		"""devuelve una lista de adyacentes del vertice pasado por parametro,
+		si no tiene ninguno devuelve lista vacia"""
+		return self.grafo[vertice]
+
+	def inicio_aleatorio(self):
+		"""elige un vertice aleatorio para iniciar el recorrido"""
+		return random.choice(self.vertice)
+
+	def agregar_vertice_con_ady(self, nuevo, ady):
+		"""agrega un vertice nuevo al grafo, adyacentes es una lista"""
+		self.grafo[nuevo] = ady
+		if self.dirigido == False:
+			for elem in ady:
+				if elem in self.grafo:
+					(self.grafo[elem]).append(nuevo)
+
+	def agregar_vertice(self,vertice)
+		if not vertice in self.grafo:
+			self.grafo[vertice] = []
+
+	def agregar_arista(self, origen, vecino):
+		"""agrega arista entre dos vertices del grafo"""
+		if origen in self.grafo:
+			self.grafo[origen].append(vecino)
+
+		if not origen in self.grafo:
+			self.grafo[origen] = [vecino]
+
+		if vecino in self.grafo:
+			self.grafo[vecino].append(origen)
+
+		if not vecino in self.grafo:
+			self.grafo[vecino] = [origen]
+
+	def son_adyacentes(self, vertice, adyacente):
+		"""devuelve true si algunos de los parametros se encuentra en la lista de
+		adyacencias del otro, independientemente de que sea dirigido o no"""
+		return (adyacente in self.grafo[vertice]) or (vertice in self.grafo[adyacente])
+
+	def existe_vertice(self, vertice):
+		"""comprueba si un vertice se encuentra en el grafo"""
+		return (self.grafo).has_key(vertice)
+
+	def cantidad_vertices(self):
+		"""devuelve la cantidad de vertices en el grafo"""
+		return len(self.grafo)
+
+	def bfs(self, inicio, cant_corte = self.cantidad_vertices()):
+		"""itera en anchura la cantidad de vertices, totales o las que se pasen por parametro"""
+		visitados, pila = [], [inicio]
+		cont = 0
+		while pila and not cont == cant_corte:
+			actual = pila.pop()
+			for w in self.adyacentes(actual):
+				if not w in visitados:
+					visitados.append(w)
+					pila.append(w)
+					cont += 1
+		return visitados
+
+	def dfs(self, actual, visitados = [], cant_corte = self.cantidad_vertices(), cont = 1):
+		"""itera en profundidad, """
+		if cont == cant_cor:
+			return visitados
+		visitados.append(actual)
+		for w in self.adyacentes(actual)
+			self.dfs(w,visitados,cant_corte,cont +1)
